b04608cc9a237b29dd5bcce636714f654a28cc06
Frank Thomas <frankthomaseng@gmail.com>
2017-08-30 17:46:26 +0300

    css

--- share.php
+++ share.php
@@ -5,7 +5,7 @@
 
 ?>
 <form>
-<input nsme="name" type="text" placeholder="your name or email" required/><br>
+<input name="name" type="text" placeholder="your name or email" required/><br>
 <input name="number_of_share_per_month" type="" placeholder="number of share bougtht per month" required><br>
 <input name="amount_of _share_bought" type="money_format" placeholder="amount of share bought" required/><br>
 <input name="date_bougth" type="date" placeholder="date share bought" required/><br>
